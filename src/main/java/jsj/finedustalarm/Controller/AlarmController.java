396282d7f22e589ef0c9801e5f2a7eda3e669115
jsj <wjd_tnwjd@naver.com>
2024-03-31 15:08:45 +0900

    웹훅 수신 서버 따로 생성 발신 서버: 8080포트 수신 서버: 9090포트

--- src/main/java/jsj/finedustalarm/Controller/AlarmController.java
+++ src/main/java/jsj/finedustalarm/Controller/AlarmController.java
@@ -25,38 +25,28 @@ import java.util.List;
 public class AlarmController {
     private final DustWarningServiceImpl dustWarningService;
 
-    @Scheduled(cron = "30 23 * * * *") // 매일 밤 11시 30분
-    private void checkAlarm() {
+//    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "0 5 * ? * * *") // 매시간 5분
+    private void checkAlarm() throws URISyntaxException, IOException, InterruptedException {
         System.out.println("실행 시간 >> " + LocalDateTime.now());
-//        List<DustAlarm> result = dustWarningService.searchByDate(LocalDateTime.parse("2023-03-10 19:29:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+//        List<DustAlarm> result = dustWarningService.searchByDate(LocalDateTime.parse("2023-03-19 21:29:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         List<DustAlarm> result = dustWarningService.searchByDate(LocalDateTime.now());
-        if (!result.isEmpty()) sendAlarm(result);
+        if (!result.isEmpty()) ringAlarm(result);
     }
 
-    private static void ringAlarm(List<DustAlarm> alarm) throws URISyntaxException, IOException {
+    private static void ringAlarm(List<DustAlarm> alarm) throws URISyntaxException, IOException, InterruptedException {
 
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonAlarm = objectMapper.writeValueAsString(alarm);
 
-        HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:8080/dust-alarm"))
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:9090/dust-alarm"))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(jsonAlarm))
                 .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Response code >> " + response.statusCode());
+        System.out.println("Response body >> " + response.body());
     }
-
-    @PostMapping("/dust-alarm")
-    public ResponseEntity<String> sendAlarm(@RequestBody List<DustAlarm> dustAlarm) {
-
-        System.out.println("====================================== (초)미세먼지 경보 알리미 ======================================\n");
-        for (DustAlarm alarm : dustAlarm) {
-            System.out.println("    ※ [" + alarm.getStationName() + "]에서 [" +
-                    alarm.getInspectDate().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분")) +
-                    "]에 [" + (alarm.getAlertGrade() == 1 ? "초미세먼지 경보" : alarm.getAlertGrade() == 2 ? "미세먼지 경보" : alarm.getAlertGrade() == 3 ? "초미세먼지 주의보" : "미세먼지 주의보") + "]가 발령 되었습니다.");
-            System.out.println();
-        }
-        System.out.println("===================================================================================================");
-        return ResponseEntity.ok("알림이 성공적으로 전송됨");
-    }
-
 }
--- src/main/java/jsj/finedustalarm/Repository/DustAlarmRepository.java
+++ src/main/java/jsj/finedustalarm/Repository/DustAlarmRepository.java
@@ -3,6 +3,7 @@ package jsj.finedustalarm.Repository;
 import jsj.finedustalarm.Entity.DustAlarm;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -10,8 +11,9 @@ import java.util.List;
 
 @Repository
 public interface DustAlarmRepository extends CrudRepository<DustAlarm, Long> {
-    @Query(value = "SELECT * FROM DUST_ALARM_HISTORY " +
+    @Query(value = "SELECT * " +
+            "FROM DUST.DUST_ALARM_HISTORY " +
             "WHERE TO_CHAR(INSPECT_DATE, 'YYYY-MM-DD') = TO_CHAR(:localDateTime, 'YYYY-MM-DD') " +
-            "ORDER BY INSPECT_DATE, STATION_CODE", nativeQuery = true)
-    List<DustAlarm> findDustAlarmByInspectDate(LocalDateTime localDateTime); // 날짜만 비교
+            "AND TO_CHAR(INSPECT_DATE, 'HH24') = TO_CHAR(:localDateTime, 'HH24')", nativeQuery = true)
+    List<DustAlarm> findDustAlarmByInspectDate(@Param("localDateTime") LocalDateTime localDateTime); // 날짜만 비교
 }
